2e34dfb4b7cb8abf40e9f8bcebee43fb6cabc382
Onurcan-Aktas <aktasonurcan420@gmail.com>
2026-03-16 14:20:43 +0300

    Initial commit

---
+++ Core/UdemyCarBook.Domain/Entities/CarFeature.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class CarFeature
+    {
+        public int CarFeatureId { get; set; }
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+        public int FeatureId { get; set; }
+        public Feature Feature { get; set; }
+        public bool Avaliable { get; set; }
+    }
+}
---
+++ Core/UdemyCarBook.Domain/Entities/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Domain.Entities
+{
+    public class Location
+    {
+        public int LocationID { get; set; }
+
+        public string Name { get; set; }
+
+    }
+}
