bea8d501207a64dabccdebf8456804251393c179
Onurcan-Aktas <aktasonurcan420@gmail.com>
2026-03-17 23:21:29 +0300

    Second commit

---
+++ Core/UdemyCarBook.Application/Features/CQRS/Commands/BrandCommands/UpdateBrandCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdemyCarBook.Application.Features.CQRS.Commands.BrandCommands
+{
+    internal class UpdateBrandCommand
+    {
+    }
+}
